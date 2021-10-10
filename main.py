f1ec05992f91c919d65dde970a7190f3bbffc203
Asandei Stefan <52822996+NikolaTesla13@users.noreply.github.com>
2021-10-10 21:24:31 +0300

    Pushing the first version of the script

---
+++ main.py
@@ -0,0 +1,58 @@
+import sys
+import socket
+import threading
+
+opened_ports = []
+socket_instance = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mutex = threading.Lock()
+
+
+def is_valid_ipv4_address(address):
+    try:
+        socket.inet_pton(socket.AF_INET, address)
+    except AttributeError:
+        try:
+            socket.inet_aton(address)
+        except socket.error:
+            return False
+        return address.count('.') == 3
+    except socket.error:
+        return False
+
+    return True
+
+
+def check_port(target, port):
+    mutex.acquire()
+    try:
+        if socket_instance.connect_ex((target, port)) == 0:
+            opened_ports.append(port)
+    finally:
+        mutex.release()
+
+
+def main():
+
+    target = '' # provide a domain or an ipv4 address
+
+    if is_valid_ipv4_address(target) == False:
+        try:
+            target = socket.gethostbyname(target)
+            socket_instance.settimeout(2)
+        except socket.error as err:
+            print("socket creation failed with error %s" % (err))
+            sys.exit()
+
+    for port in range(2998, 3004):
+        try:
+            threading.Thread(target=check_port, args=(target, port)).start()
+        except:
+            print("Error: unable to start thread")
+
+    print(opened_ports)
+
+    socket_instance.close()
+
+
+if __name__ == '__main__':
+    main()
